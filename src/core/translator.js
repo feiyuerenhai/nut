8ae4f047033d808b8f3b78d147896a8a5e2ac6c1
tony.yht <tony.yht@alibaba-inc.com>
2018-03-19 20:10:08 +0800

    initial commit

--- src/core/translator.js
+++ src/core/translator.js
@@ -11,6 +11,30 @@ const ASTtoEs5 = ast => {
 	return module.code;
 };
 
+// babel-plugin-transform-async-generator-functions
+// babel-plugin-transform-async-to-generator
+// babel-plugin-transform-es2015-arrow-functions
+// babel-plugin-transform-es2015-block-scoped-functions
+// babel-plugin-transform-es2015-block-scoping
+// babel-plugin-transform-es2015-classes
+// babel-plugin-transform-es2015-computed-properties
+// babel-plugin-transform-es2015-destructuring
+// babel-plugin-transform-es2015-duplicate-keys
+// babel-plugin-transform-es2015-for-of
+// babel-plugin-transform-es2015-function-name
+// babel-plugin-transform-es2015-literals
+// babel-plugin-transform-es2015-modules-amd
+// babel-plugin-transform-es2015-object-super
+// babel-plugin-transform-es2015-parameters
+// babel-plugin-transform-es2015-shorthand-properties
+// babel-plugin-transform-es2015-sticky-regex
+// babel-plugin-transform-es2015-template-literals
+// babel-plugin-transform-es2015-typeof-symbol
+// babel-plugin-transform-es2015-unicode-regex
+// babel-plugin-transform-exponentiation-operator
+// babel-plugin-transform-react-jsx-source
+// babel-plugin-transform-regenerator
+
 const Es7toEs5 = code => {
 	let module = babel.transform(code, {
 		"presets": [babel_env],
